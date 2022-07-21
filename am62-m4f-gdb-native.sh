7aa443d4810cf522f8cd131a3887349715e40578
Nishanth Menon <nm@ti.com>
2022-07-20 20:03:39 -0500

    add some simple scripts to run openocd and debug with gdb
    Signed-off-by: Nishanth Menon <nm@ti.com>

---
+++ am62-m4f-gdb-native.sh
@@ -0,0 +1,1 @@
+gdb-multiarch --eval-command="set architecture arm" --eval-command="target remote localhost:3339" test.elf 
---
+++ am62-run-openocd-native.sh
@@ -0,0 +1,1 @@
+sudo /usr/local/bin/openocd -f interface/ti_k3_am625-swd-native.cfg
